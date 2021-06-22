37f3ae8599b79684f078b60881222095783276e8
Anderson <andersonfonseca1999@hotmail.com>
2021-06-21 22:23:51 -0300

    Add theme and global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent
+} from 'styled-components'
+
+type GlobalStylesProps = {}
+
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+      background: ${theme.colors.bgWhite};
+      color: ${theme.colors.black};
+    }
+  `}
+`
+
+export default GlobalStyles
