57ad0a243fb724ddf0d0cf2d6cc6223ddef10dc3
Anderson <andersonfonseca1999@hotmail.com>
2021-06-23 00:34:34 -0300

    Add Home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,82 @@
+import { FormEvent, useState } from 'react';
+import * as S from './styles';
+
+import { useHistory } from 'react-router-dom';
+
+import illustrationImg from '../../assets/images/illustration.svg';
+import logoImg from '../../assets/images/logo.svg';
+import googleIconImg from '../../assets/images/google-icon.svg';
+
+import Button from '../../components/Button';
+
+import { useAuth } from '../../hooks/useAuth';
+import { database } from '../../services/firebase';
+
+function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
+    history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(e: FormEvent) {
+    e.preventDefault();
+
+    if (roomCode.trim() === '') return;
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert('Room does not exists.');
+
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Aside>
+        <S.IllustrationImg
+          src={illustrationImg}
+          alt='Ilustração simbolizando perguntas e respostas'
+        />
+        <S.Title>Crie salas de Q&amp;A ao-vivo</S.Title>
+        <S.SubTitle>Tire as dúvidas da sua audiência tem tempo-real</S.SubTitle>
+      </S.Aside>
+
+      <S.Main>
+        <S.Content>
+          <S.LogoImg src={logoImg} alt='letmeask' />
+
+          <S.GoogleButton onClick={handleCreateRoom}>
+            <S.GoogleImgIcon src={googleIconImg} alt='Logo da Google' />
+            Crie sua sala com o Google
+          </S.GoogleButton>
+
+          <S.Separator>ou entre em uma sala</S.Separator>
+
+          <S.Form onSubmit={handleJoinRoom}>
+            <S.Input
+              type='text'
+              placeholder='Digite o código da sala'
+              value={roomCode}
+              onChange={(e) => setRoomCode(e.target.value)}
+            />
+
+            <Button>Entrar na sala</Button>
+          </S.Form>
+        </S.Content>
+      </S.Main>
+    </S.Wrapper>
+  );
+}
+
+export default Home;
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,135 @@
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+`
+
+export const Aside = styled.aside`
+  ${({theme}) => css`
+    flex: 7;
+    background: ${theme.colors.purple};
+    color: ${theme.colors.white};
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 12rem 8rem;
+  `}
+`
+
+export const IllustrationImg = styled.img`
+  max-width: 32rem;
+`
+
+export const Title = styled.strong`
+  ${({theme}) => css`
+    font: ${theme.font.bold} ${theme.font.sizes.huge} 'Poppins', sans-serif;
+    line-height: 4.2rem;
+    margin-top: ${theme.spacings.xsmall};
+  `}
+`
+
+export const SubTitle = styled.p`
+  ${({theme}) => css`
+    font-size: ${theme.font.sizes.xxlarge};
+    line-height: 3.2rem;
+    margin-top: ${theme.spacings.xsmall};
+    color: ${theme.colors.textWhite};
+  `}
+`
+
+export const Main = styled.main`
+  ${({theme}) => css`
+    flex: 8;
+    padding: 0 ${theme.spacings.medium};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `}
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 32rem;
+  align-items: stretch;
+  text-align: center;
+`
+
+export const LogoImg = styled.img`
+  align-self: center;
+`
+
+export const GoogleButton = styled.button`
+  ${({theme}) => css`
+    margin-top: ${theme.spacings.huge};
+    height: 5rem;
+    border-radius: ${theme.border.radius};
+    font-weight: 500;
+    background: ${theme.colors.red};
+    color: ${theme.colors.white};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    border: 0;
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9)
+    }
+  `}
+`
+
+export const GoogleImgIcon = styled.img`
+  ${({theme}) => css`
+    margin-right: ${theme.spacings.xxsmall};
+  `}
+`
+
+export const Separator = styled.div`
+  ${({theme}) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.mediumGray};
+    margin: ${theme.spacings.medium} 0;
+    align-items: center;
+    display: flex;
+
+    &::before {
+      content: '';
+      flex: 1;
+      height: 0.1rem;
+      background: ${theme.colors.mediumGray};
+      margin-right: ${theme.spacings.xsmall};
+    }
+
+    &::after {
+      content: '';
+      flex: 1;
+      width: 100%;
+      height: 0.1rem;
+      background: ${theme.colors.mediumGray};
+      margin-left: ${theme.spacings.xsmall};
+    }
+  `}
+`
+
+export const Form = styled.form`
+  ${({theme}) => css`
+    button {
+      margin-top: ${theme.spacings.xsmall};
+    }
+  `}
+`
+
+export const Input = styled.input`
+  ${({theme}) => css`
+    width: 100%;
+    height: 5rem;
+    border-radius: ${theme.border.radius};
+    padding: 0 ${theme.spacings.xsmall};
+    background: ${theme.colors.white};
+    border: 0.1rem solid ${theme.colors.mediumGray};
+  `}
+`
