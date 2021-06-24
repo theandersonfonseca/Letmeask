9970c6ec1438e1e7a259c3e3b311e8fb41475ded
Anderson <andersonfonseca1999@hotmail.com>
2021-06-24 17:11:54 -0300

    Add modal to delete questions and end room

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -1,13 +1,16 @@
+import { useEffect, useState } from 'react';
 import * as S from './styles';
 
-import { useHistory, useParams } from 'react-router-dom';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import { database } from '../../services/firebase';
 
 import logoImg from '../../assets/images/logo.svg';
 import deleteImg from '../../assets/images/delete.svg';
+import closeImg from '../../assets/images/close.svg';
 import Button from '../../components/Button';
 import RoomCode from '../../components/RoomCode';
 import Question from '../../components/Question';
+import CustomModal from '../../components/CustomModal';
 
 import toast from 'react-hot-toast';
 
@@ -17,32 +20,114 @@ type RoomParams = {
   id: string;
 };
 
+type modalConfigType = {
+  confirmFn: Function;
+  icon: string;
+  title: string;
+  text: string;
+  confirmButtonText: string;
+};
+
+const modalConfigs = {
+  endRoom: (fn: Function) => ({
+    confirmFn: fn,
+    icon: closeImg,
+    title: 'Encerrar sala',
+    text: 'Tem certeza que você deseja encerrar esta sala?',
+    confirmButtonText: 'Sim, encerrar',
+  }),
+
+  deleteQuestion: (fn: Function) => ({
+    confirmFn: fn,
+    icon: deleteImg,
+    title: 'Excluir pergunta',
+    text: 'Tem certeza que você deseja excluir esta pergunta?',
+    confirmButtonText: 'Sim, excluir',
+  }),
+};
+
 function AdminRoom() {
   const history = useHistory();
   const { id: roomId } = useParams<RoomParams>();
-  const { questions, title } = useRoom(roomId);
+  const { questions, title, isAuthor, isClosed } = useRoom(roomId);
+  const [openModal, setOpenModal] = useState(false);
+  const [modalConfig, setModalConfig] = useState({} as modalConfigType);
+
+  const [deleteQuestion, setDeleteQuestion] = useState(false);
+  const [questionToBeDeleted, setQuestionToBeDeleted] = useState('');
+
+  const [endRoom, setEndRoom] = useState(false);
 
   async function handleEndRoom() {
-    await database.ref(`rooms/${roomId}`).update({
-      endedAt: new Date(),
-    });
-
-    history.push('/');
-    toast.success('Sala encerrada.');
+    setOpenModal(true);
+    setModalConfig(modalConfigs.endRoom(setEndRoom));
   }
 
   async function handleDeleteQuestion(questionId: string) {
-    if (window.confirm('certeza?')) {
-      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
-      toast.success('Pergunta removida.');
-    }
+    setOpenModal(true);
+    setQuestionToBeDeleted(questionId);
+    setModalConfig(modalConfigs.deleteQuestion(setDeleteQuestion));
   }
+
+  useEffect(() => {
+    async function deleteQuestionFn() {
+      if (deleteQuestion) {
+        await database
+          .ref(`rooms/${roomId}/questions/${questionToBeDeleted}`)
+          .remove();
+
+        setOpenModal(false);
+        setDeleteQuestion(false);
+        toast.success('Pergunta removida.');
+      }
+    }
+
+    deleteQuestionFn();
+  }, [questionToBeDeleted, roomId, deleteQuestion]);
+
+  useEffect(() => {
+    async function endRoomFn() {
+      if (endRoom) {
+        await database.ref(`rooms/${roomId}`).update({
+          endedAt: new Date(),
+        });
+
+        setOpenModal(false);
+        setEndRoom(false);
+        history.push('/');
+        toast.success('Sala encerrada.');
+      }
+    }
+
+    endRoomFn();
+  }, [roomId, endRoom, history]);
+
+  useEffect(() => {
+    const hasLoadedRoomInfo = isAuthor !== undefined && isClosed !== undefined;
+
+    if (isClosed && hasLoadedRoomInfo) {
+      history.push('/');
+
+      return;
+    }
+
+    if (!isAuthor && hasLoadedRoomInfo) {
+      history.push(`/rooms/${roomId}`);
+      toast.error('Você não é o administrador desta sala.');
+    }
+  }, [isAuthor, isClosed, history, roomId]);
 
   return (
     <S.Wrapper>
+      {openModal && (
+        <CustomModal isOpen setOpenModal={setOpenModal} {...modalConfig} />
+      )}
+
       <S.Header>
         <S.Content>
-          <S.LogoImg src={logoImg} alt='Letmeask' />
+          <Link to='/'>
+            <S.LogoImg src={logoImg} alt='Letmeask' />
+          </Link>
 
           <S.ButtonsWrapper>
             <RoomCode code={roomId} />
