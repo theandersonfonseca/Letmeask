0eb53bda9a9b11cacd7ef1bb380fe05a741da8a9
Anderson <andersonfonseca1999@hotmail.com>
2021-06-24 22:40:04 -0300

    Add App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+import { Toaster } from 'react-hot-toast';
+
+import Home from './pages/Home';
+import NewRoom from './pages/NewRoom';
+import Room from './pages/Room';
+import AdminRoom from './pages/AdminRoom';
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <AuthContextProvider>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/rooms/new' component={NewRoom} />
+            <Route path='/rooms/:id' component={Room} />
+            <Route path='/admin/rooms/:id' component={AdminRoom} />
+          </Switch>
+          <Toaster />
+        </AuthContextProvider>
+      </Router>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
 
 export default App;
